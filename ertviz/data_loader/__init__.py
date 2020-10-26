057075d8a6742a588ffe93adcb37487b8c32c30f
Åsmund Birkeland <musiv@equinor.com>
2020-10-26 15:36:48 +0100

    initial tests for auto connect

--- ertviz/data_loader/__init__.py
+++ ertviz/data_loader/__init__.py
@@ -3,16 +3,25 @@ import requests
 from datetime import datetime
 import logging
 import pandas
+import json
 
 
 os.environ["NO_PROXY"] = "localhost,127.0.0.1"
 
 data_cache = {}
+print(os.getcwd())
+with open("/private/musiv/tmp/ert-data/storage_server.json")  as f:
+    auth = json.load(f)
 
+def get_url():
+    return  auth["urls"][0]
+
+def get_auth():
+    return ("__token__", auth["authtoken"])
 
 def get_data(data_url):
     logging.info(f"Getting data from {data_url}...")
-    http_response = requests.get(data_url)
+    http_response = requests.get(data_url, auth=get_auth())
     http_response.raise_for_status()
 
     logging.info(" done!")
@@ -25,24 +34,25 @@ def get_numeric_data(data_url):
 
 
 def get_csv_data(data_url):
-    return pandas.read_csv(data_url, names=["value"])
+    response = requests.get(data_url,auth=get_auth(), stream=True)
+    return pandas.read_csv(response.raw, names=["value"])
 
 
 def get_ensembles():
-    server_url = "http://127.0.0.1:5000"
+    server_url = get_url()
     data_cache["ensembles"] = get_schema(f"{server_url}/ensembles")["ensembles"]
     return data_cache["ensembles"]
 
 
 def get_ensemble_url(ensemble_id):
-    server_url = "http://127.0.0.1:5000"
+    server_url = get_url()
     url = f"{server_url}/ensembles/{ensemble_id}"
     return url
 
 
 def get_schema(api_url):
     logging.info(f"Getting json from {api_url}...")
-    http_response = requests.get(api_url)
+    http_response = requests.get(api_url, auth=get_auth())
     http_response.raise_for_status()
 
     logging.info(" done!")
